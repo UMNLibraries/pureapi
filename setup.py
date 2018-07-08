78cde5c704d6f6f273e2401a7f1e1cf9286488e7
David Naughton <naughton@umn.edu>
2018-07-08 14:12:38 -0500

    Bumps version to 1.0.0.

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
   name='pureapi',
-  version='0.11.0',
+  version='1.0.0',
   description='Tools for working with Elsevier\'s Pure API.',
   url='https://github.com/UMNLibraries/pureapi',
   author='David Naughton',
   author_email='nihiliad@gmail.com',
-  packages=find_packages(exclude=['tests','docs'])
+  packages=find_packages(exclude=['tests','docs']),
+  install_requires=[
+    'addict',
+    'requests',
+    'tenacity',
+  ],
+  extras_require={
+    'dev': [
+      'pytest',
+      'python-dotenv',
+    ],
+  }
 )
