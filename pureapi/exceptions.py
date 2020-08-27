3b81552ce2492fe0731732714a9a8fa245cd2f2d
David Naughton <naughton@pop-os.localdomain>
2020-08-27 14:24:04 -0500

    Adds docstrings to exceptions, and removes unused exceptions.

--- pureapi/exceptions.py
+++ pureapi/exceptions.py
@@ -1,16 +1,4 @@
 class PureAPIException(Exception):
-    pass
-
-# Deprecate this:
-class PureAPIInvalidFamilyError(ValueError, PureAPIException):
-    def __init__(self, *args, family, version, **kwargs):
-        super().__init__(f'Invalid family "{family}" for version "{version}"', *args, **kwargs)
-
-# Do we need this?
-class PureAPIResponseException(PureAPIException):
-    pass
-
-# This may be no longer necessary...
-# Deprecate this:
-class PureAPIResponseKeyError(KeyError, PureAPIResponseException):
-    pass
+    '''Base class for pureapi exceptions.client.'''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
